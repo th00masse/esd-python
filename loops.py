481fb37aaa6ed1cd44701d55ca1774f371bd545f
Thomasse Toumi Kandza <150455637+th00masse@users.noreply.github.com>
2023-11-17 13:55:41 +0100

    Update loops.py

--- loops.py
+++ loops.py
@@ -29,3 +29,42 @@ base = int(input("Entrez la base "))
 exposant = int(input("Entrez l'exposant "))
 valeur2 = puissance(base, exposant)
 print("Le résultat est :", base, "^", exposant, "=", resultat)
+
+
+#Somme des Nombres
+
+N = int(input("Entrez un nombre entier N : "))
+
+somme = 0
+
+for i in range(1, N + 1):
+    somme += i
+
+print(f"La somme des nombres de 1 à {N} est : {somme}")
+
+#Table de Multiplication
+
+N = int(input("Entrez un nombre entier N : "))
+print(f"Table de multiplication de {N} :")
+for i in range(1, 11):
+    resultat = N * i
+    print(f"{N} x {i} = {resultat}")
+
+#Compteur Pair/Impair
+
+for i in range(1, 11):
+    if i % 2 == 0:
+        print(f"{i} est un nombre pair.")
+    else:
+        print(f"{i} est un nombre impair.")
+
+
+#Factorielle
+
+N = int(input("Entrez un nombre entier N : "))
+factorielle = 1
+
+for i in range(1, N + 1):
+    factorielle *= i
+
+print(f"La factorielle de {N} est : {factorielle}")
